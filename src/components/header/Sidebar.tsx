5297d8b37026ab4d890e150c481a73d8e1c32c6e
marcelo790 <marcelocastano95@gmail.com>
2025-02-23 19:35:08 -0400

    finalizado 60

--- src/components/header/Sidebar.tsx
+++ src/components/header/Sidebar.tsx
@@ -1,8 +1,30 @@
 import { Link } from "react-router-dom";
 
 export default function Sidebar() {
+
+  const [backgroundColor, setBackgroundColor] = useState("rgba(255, 255, 255)");
+  
+    useEffect(() => {
+      const handleScroll = () => {
+        const scrollPosition = window.scrollY;
+        if (scrollPosition > 50) {
+          setBackgroundColor("rgba(255, 255, 255, 0.65)"); // Más traslúcido al bajar
+        } else {
+          setBackgroundColor("rgba(255, 255, 255)"); // Menú más sólido al estar arriba
+        }
+      };
+  
+      window.addEventListener("scroll", handleScroll);
+  
+      // Cleanup del evento al desmontar el componente
+      return () => {
+        window.removeEventListener("scroll", handleScroll);
+      };
+    }, []);
+
+    
   return (
-    <div className="sidebar">
+    <div className="sidebar" style={{backgroundColor}}>
         <Link to='/' className="sidebar-icon">
             <img src='./images/icon-carrito.png' alt="Carrito de compras" />
             <p>15</p>

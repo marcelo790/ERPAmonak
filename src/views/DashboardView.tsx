ed10bdb00f9cc32e3fb07621f3b89bd956c42bbc
AleSapiain <31262297+AleSapiain@users.noreply.github.com>
2025-09-25 02:16:59 -0400

    Update DashboardView.tsx
    Signed-off-by: AleSapiain <31262297+AleSapiain@users.noreply.github.com>

--- src/views/DashboardView.tsx
+++ src/views/DashboardView.tsx
@@ -1,7 +1,7 @@
 import SectionBanner from "@/components/sections/SectionBanner";
 import SectionProduct from "@/components/sections/SectionProduct";
-import SectionOtherService from "@/components/sections/SectionOtherService";
-import SectionOffer from "@/components/sections/SectionOffer";
+//import SectionOtherService from "@/components/sections/SectionOtherService";
+//import SectionOffer from "@/components/sections/SectionOffer";
 import SectionHowToBuy from "@/components/sections/SectionHowToBuy";
 
 export default function DashboardView() {
